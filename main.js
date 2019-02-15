3cf9f44057286a9deb690bf00ff327b436d630db
Len Minders <>
2019-02-15 10:20:00 +0200

    make nav bar sticky if the screen is large

--- main.js
+++ main.js
@@ -2,18 +2,26 @@
 
 
 window.onscroll = function() {
-    
     elevateNavBar()
 };
 
 function elevateNavBar() {
+
+    var viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+    // checks
+
+    // if the viewport is smaller than 700px then the nav bar wont be fixed and wont need to be elevated
+    if (viewportWidth < 700){
+        return;
+    }
+
+    // check if scolled down
+    if ((document.body.scrollTop < 20 && document.documentElement.scrollTop < 20)) {
+        document.getElementById("navbar").className = "";
+        return;
+    }
+
     
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-     
-      document.getElementById("navbar").className = "active";
-  } else {
-       
-      document.getElementById("navbar").className = "";
-     
-  }
+    document.getElementById("navbar").className = "active";
 }
