c1fb68a9915f76c03333b16a086b6a3aec1cd0b1
Len Minders <>
2019-02-15 10:47:31 +0200

    fix the bug where the nav bar would not elevate when the window is resized

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 
 
-
+// on window scroll check the nav bar elevation
 window.onscroll = function() {
     elevateNavBar()
+};
+
+// on window resize, check the nav bar elevation
+window.onresize = function() {
+    elevateNavBar();
 };
 
 function elevateNavBar() {
@@ -13,6 +18,7 @@ function elevateNavBar() {
 
     // if the viewport is smaller than 700px then the nav bar wont be fixed and wont need to be elevated
     if (viewportWidth < 700){
+        document.getElementById("navbar").className = "";
         return;
     }
 
